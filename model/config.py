044ad4f2af7b05a0f755cffd5e4378b2522d93a8
guixiangyu <guixiangyu1@gmail.com>
2019-09-05 08:59:22 +0800

    init

--- model/config.py
+++ model/config.py
@@ -66,9 +66,9 @@ class Config():
     dim_char = 100
 
     # glove files
-    filename_glove = "../eswiki_20180420_300d.txt"
+    filename_glove = "../dewiki_20180420_300d.txt"
     # trimmed embeddings (created from glove_filename with build_data.py)
-    filename_trimmed = "data/eswiki_20180420_300d.npz"
+    filename_trimmed = "data/dewiki_20180420_300d.npz"
     use_pretrained = True
 
     # dataset
