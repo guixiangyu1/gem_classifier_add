c6e9e3b49e3f917bbda18be517a362246f28e4dd
guixiangyu <guixiangyu1@gmail.com>
2019-09-08 17:31:21 +0800

    init

--- model/config.py
+++ model/config.py
@@ -91,7 +91,7 @@ class Config():
     dropout          = 0.5
     batch_size       = 250
     lr_method        = "adam"
-    lr               = 0.001
+    lr               = 0.005
     lr_decay         = 1.0
     clip             = -1 # if negative, no clipping
     nepoch_no_imprv  = 8
